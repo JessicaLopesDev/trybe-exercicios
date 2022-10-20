d8da29a2d04e80952700e73c5f38f557595b2dcb
Jessica Lopes <jessicalopes.dev@gmail.com>
2022-10-20 04:52:38 -0300

    if/else retornar o maior numero ok

--- fundamentos/secao-04-introducao-a-js/dia-01-primeiros-passos/script.js
+++ fundamentos/secao-04-introducao-a-js/dia-01-primeiros-passos/script.js
@@ -1,8 +1,29 @@
-const a = 5;
-const b = 4;
+const a = 8;
+const b = 7;
+const c = 9;
 
-console.log(a+b)
-console.log(a-b)
-console.log(a*b)
-console.log(a/b)
-console.log(a%b)
+// Exercicio 1 
+// console.log(a+b)
+// console.log(a-b)
+// console.log(a*b)
+// console.log(a/b)
+// console.log(a%b)
+
+
+// Exercicio 2
+// if(a > b) {
+//     console.log(a)
+// } else {
+//     console.log(b)
+// }
+
+
+// Exercicio 3
+if(a > b && a > c) {
+    console.log(a)
+} else if (b > a && b > c) {
+    console.log(b)
+} else {
+    console.log(c)
+}
+
