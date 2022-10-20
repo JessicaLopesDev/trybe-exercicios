b44f6197ced3d036a8756b7214cf04bde59890d8
Jessica Lopes <jessicalopes.dev@gmail.com>
2022-10-20 05:03:14 -0300

    if/else angulos triangulo ok

--- fundamentos/secao-04-introducao-a-js/dia-01-primeiros-passos/script.js
+++ fundamentos/secao-04-introducao-a-js/dia-01-primeiros-passos/script.js
@@ -1,6 +1,6 @@
-const a = 8;
-const b = 7;
-const c = 9;
+const a = 100;
+const b = 40;
+const c = 5;
 
 // Exercicio 1 
 // console.log(a+b)
@@ -19,11 +19,41 @@ const c = 9;
 
 
 // Exercicio 3
-if(a > b && a > c) {
-    console.log(a)
-} else if (b > a && b > c) {
-    console.log(b)
-} else {
-    console.log(c)
-}
+// if(a > b && a > c) {
+//     console.log(a)
+// } else if (b > a && b > c) {
+//     console.log(b)
+// } else {
+//     console.log(c)
+// }
+
+
+// Exercicio 4
+// if (a > 0) {
+//     console.log("positive")
+// } else if (a < 0) {
+//     console.log("negative")
+// } else {
+//     console.log("zero")
+// }
+
+
+// Exercicio 5
+// if (a < 0 || b < 0 || c < 0 ) {
+//     console.log("Erro")
+// } else if (a + b + c == 180) {
+//     console.log("true")
+// } else {
+//     console.log("false")
+// } 
+
+
+// Exercicio 3
+// Exercicio 3
+// Exercicio 3
+// Exercicio 3
+// Exercicio 3
+// Exercicio 3
+// Exercicio 3
+// Exercicio 3
 
