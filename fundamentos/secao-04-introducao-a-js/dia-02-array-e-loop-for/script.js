74901626bf50ce84464bc631750a4e4b13b435a8
Jessica Lopes <jessicalopes.dev@gmail.com>
2022-10-21 18:57:41 -0300

    Exercicio 4, 5 e 6 ok

--- fundamentos/secao-04-introducao-a-js/dia-02-array-e-loop-for/script.js
+++ fundamentos/secao-04-introducao-a-js/dia-02-array-e-loop-for/script.js
@@ -20,22 +20,44 @@ let initial = 0
 // }
 
 // const result = initial / numbers.length;
-// console.log(result);
+// // console.log(result);
 
 
-
-// Exercicio 4
-
-
-
-
-
-// Exercicio 5
+// // Exercicio 4
+// if (result > 20) {
+//     console.log("Valor maior que 20: ", result)
+// } else {
+//     console.log("Valor menor que 20: ", result)
+// }
 
 
+// Exercicio 5 (maior numero do array)
+// let higher = numbers[0]
+
+// for (let index = 1; index < numbers.length; index++) {
+//   if (numbers[index] > higher) {
+//     higher = numbers[index];
+//   }
+// }
+// console.log(higher);
 
 
-// Exercicio 6
+// Exercicio 6 (quantos valores impares)
+let odd = 0
+
+for (let index = 0; index < numbers.length; index++) {
+  if (numbers[index] % 2 !== 0) {
+    odd += 1
+  }
+}
+
+if (odd === 0) {
+  console.log("nenhum valor ímpar encontrado")
+} else {
+  console.log(odd);
+}
+
+
 // Exercicio 1
 // Exercicio 1
 // Exercicio 1
