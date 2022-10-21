8db28f641473d492aadc1c7e4aca6ea48ac642d0
Jessica Lopes <jessicalopes.dev@gmail.com>
2022-10-21 18:21:23 -0300

    Exercicio 1, 2, 3 ok

---
+++ fundamentos/secao-04-introducao-a-js/dia-02-array-e-loop-for/script.js
@@ -0,0 +1,42 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let initial = 0
+
+// Exercicio 1
+// for (let index = 0; index < numbers.length; index++) {
+//     console.log(numbers[index]);
+// }
+
+
+// Exercicio 2
+// for (let index = 0; index < numbers.length; index++) {
+//     initial += numbers[index]
+// }
+// console.log(initial)
+
+
+// Exercicio 3
+// for (let index = 0; index < numbers.length; index++) {
+//     initial += numbers[index]
+// }
+
+// const result = initial / numbers.length;
+// console.log(result);
+
+
+
+// Exercicio 4
+
+
+
+
+
+// Exercicio 5
+
+
+
+
+// Exercicio 6
+// Exercicio 1
+// Exercicio 1
+// Exercicio 1
+// Exercicio 1
