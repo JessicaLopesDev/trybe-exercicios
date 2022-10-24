f9054e1908c1a82e3d4d15f80ea52c2c992a7abd
Jessica Lopes <jessicalopes.dev@gmail.com>
2022-10-24 14:00:00 -0300

    Exercício bonus bubble sort, 1 e 2 ok

---
+++ fundamentos/secao-04-introducao-a-js/dia-02-array-e-loop-for/bonus/script.js
@@ -0,0 +1,30 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// for (let index = 1; index < numbers.length; index += 1) {
+//   for (let secondIndex = 0; secondIndex < index; secondIndex += 1) {
+//     if (numbers[index] < numbers[secondIndex]) {
+//       let position = numbers[index];
+//       numbers[index] = numbers[secondIndex];
+//       numbers[secondIndex] = position;
+//     }
+//   }
+// }
+
+
+
+// Exercício 1
+// numbers.sort((a,b) => {
+//   if(a > b) return 1;
+//   if(a < b) return -1;
+//   return 0;
+// });
+// console.log(numbers);
+
+
+// Exercício 2
+// numbers.sort((a,b) => {
+//   if(a < b) return 1;
+//   if(a > b) return -1;
+//   return 0;
+// });
+// console.log(numbers);
