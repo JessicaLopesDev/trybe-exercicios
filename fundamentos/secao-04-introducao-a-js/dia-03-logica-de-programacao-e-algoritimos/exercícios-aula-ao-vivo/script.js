4cc1d5cdbd7a97b3eb11bd17e2672929344eed27
Jessica Lopes <jessicalopes.dev@gmail.com>
2022-10-24 18:19:24 -0300

    exercicios aula ao vivo enviados para outra pasta

---
+++ fundamentos/secao-04-introducao-a-js/dia-03-logica-de-programacao-e-algoritimos/exercícios-aula-ao-vivo/script.js
@@ -0,0 +1,39 @@
+// Exercícios em trio aula ao vivo fundamentos-seção4-dia3 (Jéssica, Bruna e Fernanda)
+
+// Exercício 1
+// let x = 0;
+
+// for (let index = 1; index <= 50; index++) {
+//     x += index
+    
+// }
+
+// console.log("A soma total de 1 a 50 é: ", x)
+
+
+// Exercício 2
+// let quantity = 0
+
+// for (let index = 2; index <= 150; index++) {
+//     if (index % 3 == 0) {
+//         quantity += 1;
+//     }
+// }
+
+// if (quantity == 50) {
+//     console.log("Essa é uma mensagem secreta");
+// }
+
+
+// Exercício 3
+let Carolzita = 22;
+let Murilo = 28;
+let Baêta = 20;
+
+if (Carolzita < Murilo && Carolzita < Baêta) {
+    console.log("Carolzita é a pessoa mais nova")
+} else if (Murilo < Carolzita && Murilo < Baêta) {
+    console.log("Murilo é a pessoa mais nova")
+} else {
+    console.log("Baêta é a pessoa mais nova")
+}
