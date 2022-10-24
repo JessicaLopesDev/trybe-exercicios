e0ac4a2121c3999809b2889d96131f37d0ffeebd
Jessica Lopes <jessicalopes.dev@gmail.com>
2022-10-24 18:15:45 -0300

    bonus ok

--- fundamentos/secao-04-introducao-a-js/dia-02-array-e-loop-for/bonus/script.js
+++ fundamentos/secao-04-introducao-a-js/dia-02-array-e-loop-for/bonus/script.js
@@ -28,3 +28,24 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 //   return 0;
 // });
 // console.log(numbers);
+
+// const n = 5;
+
+// let resultado = 0;
+// for (let index = 0; index <= n; index += 1) {
+//   resultado += index;
+// };
+// console.log(resultado)
+
+
+let element = 100
+let quantity = 0
+
+for (let index = 0; element > 0; index++) {
+  element -= 5
+  quantity = quantity +1
+  console.log("element: ", element)
+  console.log("quantity: ", quantity)
+  console.log("index: ", index)
+  
+}
