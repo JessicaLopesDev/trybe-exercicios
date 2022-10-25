6f8c7464a99e3d1df250abebc95f2a5947427365
Jessica Lopes <jessicalopes.dev@gmail.com>
2022-10-25 18:42:22 -0300

    Exercicios objetos e for/in ok

--- fundamentos/secao-04-introducao-a-js/dia-04-objetos-e-funcoes/exercicio/script.js
+++ fundamentos/secao-04-introducao-a-js/dia-04-objetos-e-funcoes/exercicio/script.js
@@ -25,26 +25,48 @@ info['recorrente'] = 'Sim';
 // }
 
 // Exercicio 5
-let info2 = {
-    personagem: 'Tio Patinhas',
-    origem: 'Christmas on Bear Mountain, Dell’s Four Color Comics #178',
-    nota: 'O último MacPatinhas’',
-    recorrente: 'Sim'
+// let info2 = {
+//     personagem: 'Tio Patinhas',
+//     origem: 'Christmas on Bear Mountain, Dell’s Four Color Comics #178',
+//     nota: 'O último MacPatinhas’',
+//     recorrente: 'Sim'
+// };
+
+// for (let i in info) {
+//     if (info[i] == info2[i]) {
+//         console.log('Ambos recorrentes');
+//     } else {
+//         console.log(info[i] + ' e ' + info2[i]);
+//     }
+// }
+
+
+// Exercicio 6 (a)
+let leitor = {
+    nome: 'Julia',
+    sobrenome: 'Pessoa',
+    idade: 21,
+    livrosFavoritos: [
+        {
+            titulo: 'O Pior Dia de Todos',
+            autor: 'Daniela Kopsch',
+            editora: 'Tordesilhas',
+        },
+    ],
 };
 
-for (let i in info) {
-    if (info[i] == info2[i]) {
-        console.log('Ambos recorrentes');
-    } else {
-        console.log(info[i] + ' e ' + info2[i]);
-    }
-}
+// console.log('O livro favorito de ' + leitor.nome + ' ' + leitor.sobrenome + ' se chama ' + '"' + leitor.livrosFavoritos[0].titulo + '"')
+
+// Exercicio 6 (b)
+leitor.livrosFavoritos[1] ={
+    titulo: 'Harry Potter e o Prisioneiro de Azkaban',
+    autor: 'JK Rowling',
+    editora: 'Rocco'}
+
+// console.log(leitor);
 
 
-// Exercicio 6
+// Exercicio 6 (c)
+console.log(leitor.nome + ' tem ' + leitor.livrosFavoritos.length + ' livros favoritos.')
 
 
-
-// Exercicio 7
-// Exercicio 
-// Exercicio 
