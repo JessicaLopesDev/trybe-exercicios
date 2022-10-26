5a4783ade5b0b3d18aca1c1524716b42f5e25a35
Jessica Lopes <jessicalopes.dev@gmail.com>
2022-10-25 22:28:10 -0300

    exercicios funções 4 e 5 ok

--- fundamentos/secao-04-introducao-a-js/dia-04-objetos-e-funcoes/exercicios-funcoes/script.js
+++ fundamentos/secao-04-introducao-a-js/dia-04-objetos-e-funcoes/exercicios-funcoes/script.js
@@ -1,20 +1,20 @@
 // Exercicio 1
 // Crie uma função que receba uma string e retorne true se for um palíndromo, ou false, se não for.
 
-function checkPalindrome (word) {
-    let reversedWord = word.split('').reverse().join('');
-    let isPalindrome = true;
+// function checkPalindrome (word) {
+//     let reversedWord = word.split('').reverse().join('');
+//     let isPalindrome = true;
 
-    if (word == reversedWord) {
-        isPalindrome = true;
-    } else {
-        isPalindrome = false;
-    }
+//     if (word == reversedWord) {
+//         isPalindrome = true;
+//     } else {
+//         isPalindrome = false;
+//     }
 
-    return isPalindrome;
-}
+//     return isPalindrome;
+// }
 
-console.log(checkPalindrome('tenet'));
+// console.log(checkPalindrome('tenet'));
 
 
 // Exercicio 2
@@ -44,23 +44,71 @@ console.log(checkPalindrome('tenet'));
 // Array de teste: [2, 4, 6, 7, 10, 0, -3];.
 // Valor esperado no retorno da função: 6.
 
-function smallerNumber (array) {
-    let counter = 0;
-    for (let i in array) {
-        if (array[counter] > array[i]) {
-            counter = i;
-        } else {
-            counter = counter;
-        }
-    }
-    return counter;
-}
+// function smallerNumber (array) {
+//     let counter = 0;
+//     for (let i in array) {
+//         if (array[counter] > array[i]) {
+//             counter = i;
+//         } else {
+//             counter = counter;
+//         }
+//     }
+//     return counter;
+// }
 
-console.log(smallerNumber([5, 2, 3, 1, 9]))
+// console.log(smallerNumber([5, 2, 3, 1, 9]))
 
 
 // Exercicio 4
+// Crie uma função que receba um array de nomes e retorne o nome com a maior quantidade de caracteres.
+// Array de teste: ['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana'];.
+// Valor esperado no retorno da função: Fernanda.
+
+// function biggerName (array) {
+//     let counter = array[0];
+//     for (let name in array) {
+//         if (counter.length < array[name].length) {
+//             counter = array[name];
+//         } else {
+//             counter = counter;
+//         }
+//     }
+//     return counter;
+// }
+
+// console.log(biggerName(['Fernanda', 'Lucas', 'Nádia']))
+
+
 // Exercicio 5
+// Crie uma função que receba um array de inteiros e retorne o inteiro que mais se repete.
+// Array de teste: [2, 3, 2, 5, 8, 2, 3];.
+// Valor esperado no retorno da função: 2.
+
+function mostRepeated(array) {
+    let outQuantity = 0;
+    let response = 0;
+    
+    for (let item of array) {
+        let repeater = item;
+        let inQuantity = 0;
+        
+        for (let item of array) {
+            if (repeater === item) {
+                inQuantity += 1;
+            }
+        }
+
+        if (inQuantity > outQuantity || repeater > response) {
+            outQuantity = inQuantity;
+            response = repeater;
+        }
+    }
+
+    return response
+}
+
+console.log('result: ', mostRepeated([4, 8, 8, 3, 3, 2, 9, 9, 2]))
+
 // Exercicio 6
 // Exercicio 7
 // Exercicio 8
