e9b64a0658727c4323df7daa897dfca14e070a58
Jessica Lopes <jessicalopes.dev@gmail.com>
2022-10-26 19:51:59 -0300

    Exercicios ok

--- fundamentos/secao-04-introducao-a-js/dia-04-objetos-e-funcoes/exercicios-funcoes/script.js
+++ fundamentos/secao-04-introducao-a-js/dia-04-objetos-e-funcoes/exercicios-funcoes/script.js
@@ -84,36 +84,72 @@
 // Array de teste: [2, 3, 2, 5, 8, 2, 3];.
 // Valor esperado no retorno da função: 2.
 
-function mostRepeated(array) {
-    let outQuantity = 0;
-    let response = 0;
+// function mostRepeated(array) {
+//     let outQuantity = 0;
+//     let response = 0;
     
-    for (let item of array) {
-        let repeater = item;
-        let inQuantity = 0;
+//     for (let item of array) {
+//         let repeater = item;
+//         let inQuantity = 0;
         
-        for (let item of array) {
-            if (repeater === item) {
-                inQuantity += 1;
-            }
-        }
+//         for (let item of array) {
+//             if (repeater === item) {
+//                 inQuantity += 1;
+//             }
+//         }
 
-        if (inQuantity > outQuantity || repeater > response) {
-            outQuantity = inQuantity;
-            response = repeater;
-        }
-    }
+//         if (inQuantity > outQuantity || repeater > response) {
+//             outQuantity = inQuantity;
+//             response = repeater;
+//         }
+//     }
 
-    return response
-}
+//     return response
+// }
 
-console.log('result: ', mostRepeated([4, 9, 8, 3, 3, 2, 9, 9, 2]))
+// console.log('result: ', mostRepeated([4, 9, 8, 3, 3, 2, 9, 9, 2]))
 
 // Exercicio 6
 // Crie uma função que receba um número natural (número inteiro não negativo) N e retorne o somatório de todos os números de 1 até N.
 // Valor de teste: N = 5.
 // Valor esperado no retorno da função: 1+2+3+4+5 = 15.
 
+// const sum = (number) => {
+//     let counter = 0;
+
+//     for (let i = 0; i <= number; i++) {
+//         counter = counter + i;
+//     }
+//     return counter;
+// }
+
+// console.log(sum(2))
 
 // Exercicio 7
-// Exercicio 8
+// Crie uma função que receba uma string word e outra string ending. Verifique se a string ending é o final da string word. Considere que a string ending sempre será menor que a string word.
+// Valor de teste: 'trybe' e 'be'
+// Valor esperado no retorno da função: true
+// verificaFimPalavra('trybe', 'be');
+// Retorno esperado: true
+// verificaFimPalavra('joaofernando', 'fernan');
+// Retorno esperado: false
+
+const endWord = (word, ending) => {
+    let separatedWord = word.split('').reverse();
+    let separatedEnding = ending.split('').reverse();
+    let result = false;
+
+    if (word.length <= ending.length) {
+        console.log('A primeira palavra precisa ser maior que a segunda palavra.');
+        return result;
+    }
+
+    for (let i = 0; i <= separatedEnding.length; i++) {
+        if (separatedEnding[i] === separatedWord[i]) {
+            result = true;
+        }
+    }
+    return result;
+}
+
+console.log(endWord('jessica', 'ess'));
