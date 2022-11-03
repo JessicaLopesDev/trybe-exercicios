13a505bd353b434cefca5a1ef876abe308f198f9
Jessica Lopes <jessicalopes.dev@gmail.com>
2022-11-03 20:34:30 -0300

    Salvando no localStorage ok

--- fundamentos/secao-05-js_dom-eventos-e-web-storage/dia-04-web-storage/exercicio-aplicando-o-dom/script.js
+++ fundamentos/secao-05-js_dom-eventos-e-web-storage/dia-04-web-storage/exercicio-aplicando-o-dom/script.js
@@ -3,13 +3,32 @@ const fontColorList = document.querySelector('#font-color').children;
 const fontSizeList = document.querySelector('#font-size').children;
 const lineHeightList = document.querySelector('#line-height').children;
 const fontFamilyList = document.querySelector('#font-family').children;
-
 const text = document.querySelector('#text');
+const body = document.getElementsByTagName('body')[0];
+const textContainer = document.querySelector('#text-container')
+
+
+const settingStorage = (key, value) => {
+    const styleValues = JSON.parse(localStorage.getItem('@styleValues'));
+
+    if (styleValues) {
+        localStorage.setItem('@styleValues', JSON.stringify({
+            ...styleValues,
+            [key]: value
+        }));
+    } else {
+        localStorage.setItem('@styleValues', JSON.stringify({
+            [key]: value
+        }));
+    } 
+}
+
 
 // Cor de fundo da tela;
 const coloringScreen = ({target}) => {
-    const body = document.getElementsByTagName('body')[0];
     body.style.background = target.innerText;
+
+    settingStorage('backgroundColor', target.innerText);
 }   
 
 for (let item of backgroundColorList) {
@@ -18,8 +37,9 @@ for (let item of backgroundColorList) {
 
 // Cor do texto;
 const changeTextColor = ({target}) => {
-    const textContainer = document.querySelector('#text-container')
     textContainer.style.color = target.innerText;
+
+    settingStorage('textColor', target.innerText);
 }   
 
 for (let item of fontColorList) {
@@ -29,6 +49,8 @@ for (let item of fontColorList) {
 // Tamanho da fonte;
 const changeFontSize = ({target}) => {
     text.style.fontSize = target.innerText;
+
+    settingStorage('fontSize', target.innerText);
 }   
 
 for (let item of fontSizeList) {
@@ -38,6 +60,8 @@ for (let item of fontSizeList) {
 // Espaçamento entre as linhas do texto;
 const changeLineHeight = ({target}) => {
     text.style.lineHeight = target.innerText;
+
+    settingStorage('lineHeight', target.innerText);
 }   
 
 for (let item of lineHeightList) {
@@ -45,3 +69,26 @@ for (let item of lineHeightList) {
 }
 
 // Tipo da fonte (Font family).
+const changeFontFamily = ({target}) => {
+    text.style.fontFamily = target.innerText;
+
+    settingStorage('fontFamily', target.innerText);
+}   
+
+for (let item of fontFamilyList) {
+    item.addEventListener('click', changeFontFamily)
+}
+
+const setConfigurationsStorage = () => {
+    const styleValues = JSON.parse(localStorage.getItem('@styleValues'));
+    
+    if (styleValues) {
+        body.style.background = styleValues.backgroundColor;
+        textContainer.style.color = styleValues.textColor;
+        text.style.fontSize = styleValues.fontSize;
+        text.style.lineHeight = styleValues.lineHeight;
+        text.style.fontFamily = styleValues.fontFamily;
+    }
+}
+
+window.onload = setConfigurationsStorage
