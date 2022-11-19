519d21d1057dab885b97913c2586aaa75ce6209c
Jessica Lopes <jessicalopes.dev@gmail.com>
2022-11-19 17:47:34 -0300

    Parte 1 resolvida

--- fundamentos/secao-07-introducao-a-jsES6-e-testes-unitarios/dia-01-fluxo-de-excecao-e-objetos/script.js
+++ fundamentos/secao-07-introducao-a-jsES6-e-testes-unitarios/dia-01-fluxo-de-excecao-e-objetos/script.js
@@ -1,11 +1,29 @@
-function sum() {
+const validateInputValues = (number1, number2) => {
+    if (!number1 || !number2) {
+        throw new Error('Você precisa informar os 2 valores!');
+    }
+    if (isNaN(number1) || isNaN(number2)) {
+        throw new Error('Os valores precisam ser numéricos!');
+    }
+}
+
+const sum = () => {
     const value1 = document.getElementById('value1').value;
     const value2 = document.getElementById('value2').value;
-    const result = Number(value1) + Number(value2);
-    document.getElementById('result').innerHTML = `Resultado: ${result}`;
-    document.getElementById('value1').value = '';
-    document.getElementById('value2').value = '';
+    try {
+        validateInputValues(value1, value2);
+        const result = Number(value1) + Number(value2);
+        document.getElementById('result').innerHTML = `Resultado: ${result}`;
+    } catch (error) {
+        document.getElementById('result').innerHTML = `Resultado: ${error.message}`;
+        console.error(error.message);
+    } finally {
+        document.getElementById('value1').value = '';
+        document.getElementById('value2').value = '';
+    }
 }
+
+
 window.onload = () => {
     const button = document.getElementById('button');
     button.addEventListener('click', sum);
