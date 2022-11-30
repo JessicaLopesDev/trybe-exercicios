f91890309b46707bf5b44ef2e3931bdb295a4224
Jessica Lopes <jessicalopes.dev@gmail.com>
2022-11-30 15:52:44 -0300

    Separando exercicios em arquivos

--- fundamentos/secao-08-hof-do-jsES6/dia-01-introducao-hof-/exercise3.js
+++ fundamentos/secao-08-hof-do-jsES6/dia-01-introducao-hof-/exercise3.js
@@ -61,27 +61,6 @@ const books = [
   },
 ];
 
-// const expectedResult = 'Stephen King';
-const authorBornIn1947 = (array) => {
-  return array.find(item => item.author.birthYear === 1947).author.name;
-}
-// console.log(authorBornIn1947(books));
-
-
-// const expectedResult = 'Duna';
-const smallerName = (array) => {
-  let nameBook;
-
-  array.forEach(item => {
-    if (!nameBook || item.name.length < nameBook.length) {
-      nameBook = item.name;
-    };
-  });
-  return nameBook;
-}
-// console.log(smallerName(books));
-
-
 // const expectedResult = {
 //   author: {
 //     birthYear: 1948,
@@ -95,36 +74,4 @@ const smallerName = (array) => {
 const getNamedBook = (array) => {
   return array.find(item => item.name.length === 26);
 }
-// console.log(getNamedBook(books));
-
-
-const expectedResult = false;
-function everyoneWasBornOnSecXX(array) {  
-  return array.every(item => item.author.birthYear >= 1901 && item.author.birthYear <= 2000);
-}
-// console.log(everyoneWasBornOnSecXX(books));
-
-
-// const expectedResult = true;
-const someBookWasReleaseOnThe80s = (array) => {
-  return array.some(item => item.releaseYear > 1980 && item.releaseYear < 1989);
-}
-// console.log(someBookWasReleaseOnThe80s(books));
-
-
-// const expectedResult = false;
-function authorUnique(array) {
-  let firstBirthYear = 1;
-  
-  array.forEach((item) => {
-    if (!!firstBirthYear && item.author.birthYear !== firstBirthYear) {
-      firstBirthYear = item.author.birthYear
-    } else {
-      firstBirthYear = 0;
-    }
-  })
-
-  return !!firstBirthYear;
-}
-
-console.log(authorUnique(books));
+console.log(getNamedBook(books));
