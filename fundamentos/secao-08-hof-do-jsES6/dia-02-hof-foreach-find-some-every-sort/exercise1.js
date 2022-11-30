a32a35978d58781c543b657350a15a470e9ba1f7
Jessica Lopes <jessicalopes.dev@gmail.com>
2022-11-30 16:23:45 -0300

    Exercicio 1 ok

--- fundamentos/secao-08-hof-do-jsES6/dia-02-hof-foreach-find-some-every-sort/exercise1.js
+++ fundamentos/secao-08-hof-do-jsES6/dia-02-hof-foreach-find-some-every-sort/exercise1.js
@@ -61,3 +61,25 @@ const books = [
   },
 ];
 
+//Crie um array com strings no formato NOME_DO_LIVRO - GÊNERO_DO_LIVRO - NOME_DA_PESSOA_AUTORA
+
+// const expectedResult = [
+//   'As Crônicas de Gelo e Fogo - Fantasia - George R. R. Martin',
+//   'O Senhor dos Anéis - Fantasia - J. R. R. Tolkien',
+//   'Fundação - Ficção Científica - Isaac Asimov',
+//   'Duna - Ficção Científica - Frank Herbert',
+//   'A Coisa - Terror - Stephen King',
+//   'O Chamado de Cthulhu - Terror - H. P. Lovecraft',
+// ];
+
+const formatedBookNames = (array) => {
+  const result = array.map((element) => {
+    let name = element.name.toUpperCase();
+    let genre = element.genre.toUpperCase();
+    let author = element.author.name.toUpperCase();
+    return `${name} - ${genre} - ${author}`
+  });
+  return result;
+};
+
+console.log(formatedBookNames(books));
